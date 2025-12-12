b3fe72af0f2f28991503dc4d387337d8ebaa0531
Anish Kshatriya <obliviga@gmail.com>
2025-12-12 12:39:37 -0800

    Replace Astro MD hardcoded blog with content from Sanity

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,18 @@
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+
+// This file is intentionally left empty.
+// It's a good idea to keep it around in case you need to add any global types.
+export type Post = {
+  _id: string;
+  _type: "post";
+  _createdAt: string;
+  title: string;
+  slug: {
+    _type: "slug";
+    current: string;
+  };
+  image: any;
+  publishedAt: string;
+  body: any;
+};
